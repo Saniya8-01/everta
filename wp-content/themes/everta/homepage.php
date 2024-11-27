ab0cdcee7e50a1454d34fd8a74d0e3187cf16b5c
sdurbule27 <shamal.durbule@theminimalist.in>
2024-11-27 02:34:06 -0800

    faq section

--- wp-content/themes/everta/homepage.php
+++ wp-content/themes/everta/homepage.php
@@ -348,5 +348,69 @@
     </div>
 </section>
 
+<section class="faqSection" id="faqAccordion">
+    <div class="faq-heading">
+        <h2>
+            Frequently Asked Questions </h2>
+    </div>
+    <div class="container">
+        <div class="accordions-wrapper">
+            <div class="accordion active">
+                <div class="accordion-header">
+                    <h4>How does Coverstack’s Lending Stack integrate with my existing lending workflow? </h4>
+                    <div class="accordion-icon"></div>
+                </div>
+                <div class="accordion-body" style="max-height: fit-content;">
+                    <p>
+                        Coverstack’s Lending Stack is designed for seamless integration into your existing lending
+                        processes, allowing you to quickly launch and scale insurance offerings without disrupting your
+                        current workflow. Our platform enables you to embed insurance solutions effortlessly into your
+                        lending operations </p>
+                </div>
+            </div>
+            <div class="accordion">
+                <div class="accordion-header">
+                    <h4>
+                        Is the Lending Stack insurer agnostic? </h4>
+                    <div class="accordion-icon"></div>
+                </div>
+                <div class="accordion-body">
+                    <p>
+                        Yes, the Lending Stack provides access to a wide range of insurance products from multiple
+                        leading insurers. This flexibility allows you to configure the best coverage options tailored
+                        specifically to your customers' needs, ensuring that you can offer the most suitable products
+                    </p>
+                </div>
+            </div>
+            <div class="accordion">
+                <div class="accordion-header">
+                    <h4>
+                        How does the platform optimize operational efficiency? </h4>
+                    <div class="accordion-icon"></div>
+                </div>
+                <div class="accordion-body">
+                    <p>
+                        Our platform streamlines your insurance processes through real-time automated policy issuance,
+                        reducing the need for manual intervention and minimizing errors. This enhances operational
+                        efficiency and ensures a smoother, more reliable insurance issuance process </p>
+                </div>
+            </div>
+            <div class="accordion">
+                <div class="accordion-header">
+                    <h4>
+                        What kind of analytics does the Lending Stack offer? </h4>
+                    <div class="accordion-icon"></div>
+                </div>
+                <div class="accordion-body">
+                    <p>
+                        The Lending Stack includes a deep analytics dashboard that provides advanced insights into your
+                        portfolio performance. You can leverage these insights to make data-driven decisions, optimize
+                        your insurance strategy, and mitigate risks effectively </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 <?php get_footer(); ?>
