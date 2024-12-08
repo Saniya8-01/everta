6e7bf8a3086123e2402722ed009a105edc98f8d0
sachin gour <sachinMinimalist001>
2024-12-08 12:21:08 +0530

    about us code

---
+++ wp-content/themes/everta/about-us.php
@@ -0,0 +1,79 @@
+<?php get_header(); /*Template name: About-us*/ ?>
+
+<section class="journeySection">
+    <div class="journeySecWrapper">
+        <div class="secHeading">
+            <h1>Seamless charging for every journey</h1>
+            <p>Discover reliable, sustainable solutions tailored to your needs</p>
+        </div>
+        <div class="imgBox">
+            <img src="<?php bloginfo('template_directory'); ?>/images/aboutus-journey-banner.webp" alt="">
+        </div>
+    </div>
+</section>
+
+<section class="missionSection">
+    <div class="missionSecWrapper">
+        <div class="mainContainer">
+            <div class="card mission">
+                <div class="hoverContent">
+                    <p>Imagine a world where fueling your car is not a shame but pure joy. That's the world we're creating as part of the e-movement. It’s not just filling batteries, but empowering sustainable human exploration. Carefree, indeed.</p>
+                    <img src="<?php bloginfo('template_directory'); ?>/images/mission-icon.svg" alt="">
+                </div>
+                <h2>MISSION</h2>
+            </div>
+            <div class="card vision">
+                <div class="hoverContent">
+                    <p>We are making the inevitable transformation painless. We empower an easier change in the way we move and the way we think. Because we just have to.</p>
+                    <img src="<?php bloginfo('template_directory'); ?>/images/vision-icon.svg" alt="">
+                </div>
+                <h2>VISION</h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="evertaEveryoneSection weValueSection">
+    <h2>AT EVERTA, WE VALUE</h2>
+    <div class="hover-section">
+        <div class="hover-box">
+            <div class="content">
+                <h3>QUALITY</h3>
+                <p>Quality is more than a goal—it’s the foundation of everything we do. We go beyond expectations in every detail and process, ensuring that our products and services consistently reflect our commitment to excellence. At Everta, quality isn’t just a standard—it’s who we are.</p>
+            </div>
+            <div class="image">
+                <img src="<?php bloginfo('template_directory'); ?>/images/everta-we-value.webp" alt="">
+            </div>
+        </div>
+        <div class="hover-box">
+            <div class="content">
+                <h3>SUSTAINABILITY</h3>
+                <p>Quality is more than a goal—it’s the foundation of everything we do. We go beyond expectations in every detail and process, ensuring that our products and services consistently reflect our commitment to excellence. At Everta, quality isn’t just a standard—it’s who we are.</p>
+            </div>
+            <div class="image">
+                <img src="<?php bloginfo('template_directory'); ?>/images/everta-we-value1.webp" alt="">
+            </div>
+        </div>
+        <div class="hover-box">
+            <div class="content">
+                <h3>INTEGRITY</h3>
+                <p>Quality is more than a goal—it’s the foundation of everything we do. We go beyond expectations in every detail and process, ensuring that our products and services consistently reflect our commitment to excellence. At Everta, quality isn’t just a standard—it’s who we are.</p>
+            </div>
+            <div class="image">
+                <img src="<?php bloginfo('template_directory'); ?>/images/everta-we-value2.webp" alt="">
+            </div>
+        </div>
+        <div class="hover-box">
+            <div class="content">
+                <h3>PEOPLE</h3>
+                <p>Quality is more than a goal—it’s the foundation of everything we do. We go beyond expectations in every detail and process, ensuring that our products and services consistently reflect our commitment to excellence. At Everta, quality isn’t just a standard—it’s who we are.</p>
+            </div>
+            <div class="image">
+                <img src="<?php bloginfo('template_directory'); ?>/images/everta-we-value3.webp" alt="">
+            </div>
+        </div>
+       
+    </div>
+</section>
+
+<?php get_footer(); ?>
