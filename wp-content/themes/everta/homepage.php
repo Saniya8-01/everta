740a0a0f1beb9ebf054ebba9a359a649dcb6d6fa
sdurbule27 <shamal.durbule@theminimalist.in>
2025-01-19 16:29:00 +0530

    homepage changes

--- wp-content/themes/everta/homepage.php
+++ wp-content/themes/everta/homepage.php
@@ -8,36 +8,36 @@
         // Get the media choice for the current section (either 'Image' or 'Video')
         $mediaChoice = get_sub_field('media_choice');
         ?>
-        
+
         <!-- Media Section: Image or Video -->
         <?php if ( $mediaChoice == 'Image' ) : ?>
-            <div class="bannerImageWrapper">
-                <?php
+        <div class="bannerImageWrapper">
+            <?php
                 $desktop_image = get_sub_field('desktop_image');
                 $mobile_image = get_sub_field('mobile_image');
                 ?>
-                <?php if ( ! empty( $desktop_image ) ): ?>
-                <img src="<?php echo esc_url( $desktop_image['url'] ); ?>"
-                    alt="<?php echo esc_attr( $desktop_image['alt'] ); ?>" class="desktopImage">
-                <?php endif; ?>
-    
-                <?php if ( ! empty( $mobile_image ) ): ?>
-                <img src="<?php echo esc_url( $mobile_image['url'] ); ?>"
-                    alt="<?php echo esc_attr( $mobile_image['alt'] ); ?>" class="mobileImage">
-                <?php endif; ?>
-                <div class="blackOverlay"></div>
-            </div>
+            <?php if ( ! empty( $desktop_image ) ): ?>
+            <img src="<?php echo esc_url( $desktop_image['url'] ); ?>"
+                alt="<?php echo esc_attr( $desktop_image['alt'] ); ?>" class="desktopImage">
+            <?php endif; ?>
+
+            <?php if ( ! empty( $mobile_image ) ): ?>
+            <img src="<?php echo esc_url( $mobile_image['url'] ); ?>"
+                alt="<?php echo esc_attr( $mobile_image['alt'] ); ?>" class="mobileImage">
+            <?php endif; ?>
+            <div class="blackOverlay"></div>
+        </div>
         <?php elseif ( $mediaChoice == 'Video' ) : ?>
-            <div class="videoDiv">
-                <video autoplay="" preload="auto" loop="" muted="" playsinline="" class="desktopVideo">
-                    <source src="<?php echo esc_url(get_sub_field('video_link')); ?>" type="video/mp4">
-                </video>
-                <video autoplay="" preload="auto" loop="" muted="" playsinline="" class="mobileVideo">
-                    <source src="<?php echo esc_url(get_sub_field('video_link_mobile')); ?>" type="video/mp4">
-                </video>
-            </div>
+        <div class="videoDiv">
+            <video autoplay="" preload="auto" loop="" muted="" playsinline="" class="desktopVideo">
+                <source src="<?php echo esc_url(get_sub_field('video_link')); ?>" type="video/mp4">
+            </video>
+            <video autoplay="" preload="auto" loop="" muted="" playsinline="" class="mobileVideo">
+                <source src="<?php echo esc_url(get_sub_field('video_link_mobile')); ?>" type="video/mp4">
+            </video>
+        </div>
         <?php endif; ?>
-    
+
         <div class="bannerSectionContent">
             <h1>
                 <?php echo get_sub_field('banner_heading'); ?>
@@ -50,10 +50,10 @@
             $banner_cta_text = get_sub_field('banner_cta_text');
             
             if ($banner_cta_link && $banner_cta_text) : ?>
-                <a href="<?php echo esc_url($banner_cta_link); ?>" class="ctaYellow">
-                    <?php echo esc_html($banner_cta_text); ?>
-                </a>
-            <?php endif; ?>            
+            <a href="<?php echo esc_url($banner_cta_link); ?>" class="ctaYellow">
+                <?php echo esc_html($banner_cta_text); ?>
+            </a>
+            <?php endif; ?>
         </div>
     </div>
     <?php endwhile; ?>
@@ -81,11 +81,11 @@
                 $explore_text = get_sub_field('explore_text');
                 
                 if ($explore_link && $explore_text) : ?>
-                    <a href="<?php echo $explore_link; ?>" class="ctaWhiteBlack">
-                        <?php echo $explore_text; ?>
-                    </a>
+                <a href="<?php echo $explore_link; ?>" class="ctaWhiteBlack">
+                    <?php echo $explore_text; ?>
+                </a>
                 <?php endif; ?>
-                
+
             </div>
         </div>
         <div class="rightContent">
@@ -110,68 +110,72 @@
 
 <section class="evertaEveryoneSection">
     <?php if (have_rows('evertaeveryone_section')) : ?>
-        <?php while (have_rows('evertaeveryone_section')) : the_row(); ?>
-            <div class="evertaEveryoneSectionWrapper">
-                <div class="evertaEveryoneSectionHeading">
-                    <h2>
-                        <?php echo get_sub_field('evertaeveryone_mainheading'); ?>
-                    </h2>
+    <?php while (have_rows('evertaeveryone_section')) : the_row(); ?>
+    <div class="evertaEveryoneSectionWrapper">
+        <div class="evertaEveryoneSectionHeading">
+            <h2>
+                <?php echo get_sub_field('evertaeveryone_mainheading'); ?>
+            </h2>
+            <p>
+                <?php echo get_sub_field('evertaeveryone_subheading'); ?>
+            </p>
+        </div>
+        <div class="hover-section">
+            <?php if (have_rows('hover_section')) : ?>
+            <?php $counter=1; while (have_rows('hover_section')) : the_row(); ?>
+            <div class="hover-box">
+                <div class="content">
+                    <h3>
+                        <?php echo get_sub_field('hovercard_title'); ?>
+                    </h3>
                     <p>
-                        <?php echo get_sub_field('evertaeveryone_subheading'); ?>
+                        <?php echo get_sub_field('hovercard_para'); ?>
                     </p>
-                </div>
-                <div class="hover-section">
-                    <?php if (have_rows('hover_section')) : ?>
-                        <?php $counter=1; while (have_rows('hover_section')) : the_row(); ?>
-                            <div class="hover-box">
-                                <div class="content">
-                                    <h3>
-                                        <?php echo get_sub_field('hovercard_title'); ?>
-                                    </h3>
-                                    <p>
-                                        <?php echo get_sub_field('hovercard_para'); ?>
-                                    </p>
-                                    <button class="toggle" data-target="myPopup<?php echo $counter; ?>">read more</button>
-                                    <?php 
+                    <button class="toggle" data-target="myPopup<?php echo $counter; ?>">read more</button>
+                    <?php 
                                         $explore_link = get_sub_field('explore_link');
                                         $explore_text = get_sub_field('explore_text');
                                         
                                         if ($explore_link && $explore_text) : ?>
-                                            <a href="<?php echo $explore_link; ?>" class="ctaYellowBlack">
-                                                <?php echo $explore_text; ?>
-                                            </a>
-                                    <?php endif; ?>
-                                </div>
-                                <div class="image">
-                                    <?php $hoverImage = get_sub_field('hover_image');
+                    <a href="<?php echo $explore_link; ?>" class="ctaYellowBlack">
+                        <?php echo $explore_text; ?>
+                    </a>
+                    <?php endif; ?>
+                </div>
+                <div class="image">
+                    <?php $hoverImage = get_sub_field('hover_image');
                                     if (!empty($hoverImage)) : ?>
-                                    <img src="<?php echo esc_url($hoverImage['url']); ?>" loading="lazy"
-                                        alt="<?php echo esc_attr($hoverImage['alt']); ?>" />
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                        <?php $counter=$counter+1; endwhile; ?>
+                    <img src="<?php echo esc_url($hoverImage['url']); ?>" loading="lazy"
+                        alt="<?php echo esc_attr($hoverImage['alt']); ?>" />
                     <?php endif; ?>
                 </div>
             </div>
-            <?php if (have_rows('hover_section')) : ?>
-                <?php $counter=1; while (have_rows('hover_section')) : the_row(); ?>
-                    <div id="myPopup<?php echo $counter; ?>" class="popup hide">
-                        <div class="overlay">
-                            <div class="popup-header">
-                                <span class="close toggle" data-target="myPopup<?php echo $counter; ?>">
-                                    <i class="icon-cross fontelloCrossIcon"></i>
-                                </span>
-                                <h3><?php echo get_sub_field('hovercard_title'); ?></h3>
-                            </div>
-                            <div class="popup-body">
-                                <p><?php echo get_sub_field('hovercard_para'); ?></p>
-                            </div>
-                        </div>
-                    </div>
-                <?php $counter=$counter+1; endwhile; ?>
+            <?php $counter=$counter+1; endwhile; ?>
             <?php endif; ?>
-        <?php endwhile; ?>
+        </div>
+    </div>
+    <?php if (have_rows('hover_section')) : ?>
+    <?php $counter=1; while (have_rows('hover_section')) : the_row(); ?>
+    <div id="myPopup<?php echo $counter; ?>" class="popup hide">
+        <div class="overlay">
+            <div class="popup-header">
+                <span class="close toggle" data-target="myPopup<?php echo $counter; ?>">
+                    <i class="icon-cross fontelloCrossIcon"></i>
+                </span>
+                <h3>
+                    <?php echo get_sub_field('hovercard_title'); ?>
+                </h3>
+            </div>
+            <div class="popup-body">
+                <p>
+                    <?php echo get_sub_field('hovercard_para'); ?>
+                </p>
+            </div>
+        </div>
+    </div>
+    <?php $counter=$counter+1; endwhile; ?>
+    <?php endif; ?>
+    <?php endwhile; ?>
     <?php endif; ?>
 </section>
 
@@ -217,13 +221,61 @@
                 <?php echo get_sub_field('subheading'); ?>
             </p>
             <div class="tab-buttons">
-                <button class="tab-button active" data-tab="ac">AC</button>
-                <button class="tab-button" data-tab="dc">DC</button>
+                <button class="tab-button active" data-tab="dc">DC</button>
+                <button class="tab-button" data-tab="ac">AC</button>
             </div>
         </div>
-
+        <!-- Tab Content for DC -->
+        <div class="tab-content active" id="dc">
+            <div class="cards">
+                <?php 
+            $related_post = get_sub_field('related_products_dc'); 
+            if ($related_post) : 
+                $counter = 0; // Initialize counter
+                foreach ($related_post as $post) : 
+                    setup_postdata($post); 
+                    $post_id = get_the_ID(); 
+                    $post_link = get_permalink($post_id);
+                    if ($counter >= 3) break; // Stop loop after 3 cards
+                    $counter++;
+            ?>
+                <div class="card">
+                    <div class="card-wrapper">
+                        <?php $full_image_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
+                        <img src="<?php echo $full_image_url[0]; ?>" alt="product-image">
+                        <h3>
+                            <?php the_title(); ?>
+                        </h3>
+                        <?php 
+                    $features = [];
+                    if (have_rows('banner_section', $post_id)) : 
+                        while (have_rows('banner_section', $post_id)) : the_row(); 
+                            if (have_rows('features_list')) : 
+                                while (have_rows('features_list')) : the_row(); 
+                                    $features[] = get_sub_field('feature_info');
+                                endwhile; 
+                            endif; 
+                        endwhile; 
+                    endif; 
+                    ?>
+                        <h5>
+                            <?php echo implode(' • ', $features); ?>
+                        </h5>
+                        <p>
+                            <?php echo wp_trim_words(get_the_content(), 40, '...'); ?>
+                        </p>
+                    </div>
+                    <a href="<?php echo esc_url($post_link); ?>">Explore more
+                        <i class="icon-right-arrow fontellowRightArrow"></i>
+                    </a>
+                </div>
+                <?php endforeach; ?>
+                <?php wp_reset_postdata(); ?>
+                <?php endif; ?>
+            </div>
+        </div>
         <!-- Tab Content for AC -->
-        <div class="tab-content active" id="ac">
+        <div class="tab-content" id="ac">
             <div class="cards">
                 <?php 
                     $related_post = get_sub_field('related_products_ac'); 
@@ -262,67 +314,12 @@
                             <?php echo wp_trim_words(get_the_content(), 40, '...'); ?>
                         </p>
                     </div>
-                    <a href="<?php echo esc_url($post_link); ?>">Explore more
-                        <!-- <img src="<?php bloginfo('template_directory'); ?>/images/yellow-cta-arrow.svg" alt=""> -->
-                         <i class="icon-right-arrow fontellowRightArrow"></i>
-                    </a>
                 </div>
                 <?php endforeach; ?>
                 <?php wp_reset_postdata(); ?>
                 <?php endif; ?>
             </div>
         </div>
-
-        <!-- Tab Content for DC -->
-        <div class="tab-content" id="dc">
-            <div class="cards">
-                <?php 
-        $related_post = get_sub_field('related_products_dc'); 
-        if ($related_post) : 
-            $counter = 0; // Initialize counter
-            foreach ($related_post as $post) : 
-                setup_postdata($post); 
-                $post_id = get_the_ID(); 
-                $post_link = get_permalink($post_id);
-                if ($counter >= 3) break; // Stop loop after 3 cards
-                $counter++;
-        ?>
-                <div class="card">
-                    <div class="card-wrapper">
-                        <?php $full_image_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
-                        <img src="<?php echo $full_image_url[0]; ?>" alt="product-image">
-                        <h3>
-                            <?php the_title(); ?>
-                        </h3>
-                        <?php 
-                $features = [];
-                if (have_rows('banner_section', $post_id)) : 
-                    while (have_rows('banner_section', $post_id)) : the_row(); 
-                        if (have_rows('features_list')) : 
-                            while (have_rows('features_list')) : the_row(); 
-                                $features[] = get_sub_field('feature_info');
-                            endwhile; 
-                        endif; 
-                    endwhile; 
-                endif; 
-                ?>
-                        <h5>
-                            <?php echo implode(' • ', $features); ?>
-                        </h5>
-                        <p>
-                            <?php echo wp_trim_words(get_the_content(), 40, '...'); ?>
-                        </p>
-                    </div>
-                    <a href="<?php echo esc_url($post_link); ?>">Explore more
-                        <img src="<?php bloginfo('template_directory'); ?>/images/yellow-cta-arrow.svg" alt="">
-                    </a>
-                </div>
-                <?php endforeach; ?>
-                <?php wp_reset_postdata(); ?>
-                <?php endif; ?>
-            </div>
-        </div>
-
     </div>
     <?php endwhile; ?>
     <?php endif; ?>
@@ -345,10 +342,10 @@
                 $solutions_text = get_sub_field('solutions_text');
                 
                 if ($solutions_link && $solutions_text) : ?>
-                    <a href="<?php echo $solutions_link; ?>" class="ctaBlack">
-                        <?php echo $solutions_text; ?>
-                    </a>
-                <?php endif; ?>                
+                <a href="<?php echo $solutions_link; ?>" class="ctaBlack">
+                    <?php echo $solutions_text; ?>
+                </a>
+                <?php endif; ?>
             </div>
         </div>
         <div class="rightContent">
